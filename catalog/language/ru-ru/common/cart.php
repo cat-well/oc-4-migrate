0b195cc20faf1f677a9634688b9ccdb2679637ee
Valentyn Kotenko <kotenko@next-level.com>
2026-02-20 00:17:31 +0100

    manline: restore OC2-style cart icon/count markup via ru/ua language text_items

---
+++ catalog/language/ru-ru/common/cart.php
@@ -0,0 +1,20 @@
+<?php
+// Text
+// Manline: keep OC2-like cart bubble + total markup for header mini-cart.
+$_['text_items']                 = '<span class="cart_qnt">%s</span><span class="cart_total">%s</span>';
+$_['text_points']                = 'Бонусные баллы';
+$_['text_subscription']          = 'Подписка';
+$_['text_subscription_duration'] = '%s каждые %d %s(ов) в течение %d платеж(ей)';
+$_['text_subscription_cancel']   = '%s каждые %d %s(ов) до отмены';
+$_['text_day']                   = 'день';
+$_['text_week']                  = 'неделя';
+$_['text_semi_month']            = 'полмесяца';
+$_['text_month']                 = 'месяц';
+$_['text_year']                  = 'год';
+$_['text_no_results']            = 'Ваша корзина пуста!';
+$_['text_model']                 = 'Модель';
+$_['text_cart']                  = 'Корзина';
+$_['text_checkout']              = 'Оформить заказ';
+
+// Error
+$_['error_product']              = 'Товар не найден!';
---
+++ catalog/language/uk-ua/common/cart.php
@@ -0,0 +1,20 @@
+<?php
+// Text
+// Manline: keep OC2-like cart bubble + total markup for header mini-cart.
+$_['text_items']                 = '<span class="cart_qnt">%s</span><span class="cart_total">%s</span>';
+$_['text_points']                = 'Бонусні бали';
+$_['text_subscription']          = 'Підписка';
+$_['text_subscription_duration'] = '%s кожні %d %s(ів) протягом %d платеж(ів)';
+$_['text_subscription_cancel']   = '%s кожні %d %s(ів) до скасування';
+$_['text_day']                   = 'день';
+$_['text_week']                  = 'тиждень';
+$_['text_semi_month']            = 'півмісяця';
+$_['text_month']                 = 'місяць';
+$_['text_year']                  = 'рік';
+$_['text_no_results']            = 'Ваш кошик порожній!';
+$_['text_model']                 = 'Модель';
+$_['text_cart']                  = 'Кошик';
+$_['text_checkout']              = 'Оформити замовлення';
+
+// Error
+$_['error_product']              = 'Товар не знайдено!';
