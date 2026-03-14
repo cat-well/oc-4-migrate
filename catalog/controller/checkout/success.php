ebce7e67ef2560112a43d88055d0d051670ad35e
Valentyn Kotenko <kotenko@next-level.com>
2026-03-15 00:57:30 +0100

    Success page: use current language in links; render OC2-style breadcrumbs

--- catalog/controller/checkout/success.php
+++ catalog/controller/checkout/success.php
@@ -32,33 +32,41 @@ class Success extends \Opencart\System\Engine\Controller {
 
 		$data['breadcrumbs'] = [];
 
+		$language = (string)($this->request->get['language'] ?? ($this->session->data['language'] ?? $this->config->get('config_language')));
+
 		$data['breadcrumbs'][] = [
 			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/home', 'language=' . $this->config->get('config_language'))
+			'href' => $this->url->link('common/home', 'language=' . $language)
 		];
 
 		$data['breadcrumbs'][] = [
 			'text' => $this->language->get('text_basket'),
-			'href' => $this->url->link('checkout/cart', 'language=' . $this->config->get('config_language'))
+			'href' => $this->url->link('checkout/cart', 'language=' . $language)
 		];
 
 		$data['breadcrumbs'][] = [
 			'text' => $this->language->get('text_checkout'),
-			'href' => $this->url->link('checkout/checkout', 'language=' . $this->config->get('config_language'))
+			'href' => $this->url->link('checkout/checkout', 'language=' . $language)
 		];
 
 		$data['breadcrumbs'][] = [
 			'text' => $this->language->get('text_success'),
-			'href' => $this->url->link('checkout/success', 'language=' . $this->config->get('config_language'))
+			'href' => $this->url->link('checkout/success', 'language=' . $language)
 		];
 
 		if ($this->customer->isLogged()) {
-			$data['text_message'] = sprintf($this->language->get('text_customer'), $this->url->link('account/account', 'language=' . $this->config->get('config_language') . '&customer_token=' . $this->session->data['customer_token']), $this->url->link('account/order', 'language=' . $this->config->get('config_language') . '&customer_token=' . $this->session->data['customer_token']), $this->url->link('account/download', 'language=' . $this->config->get('config_language') . '&customer_token=' . $this->session->data['customer_token']), $this->url->link('information/contact', 'language=' . $this->config->get('config_language')));
+			$data['text_message'] = sprintf(
+				$this->language->get('text_customer'),
+				$this->url->link('account/account', 'language=' . $language . '&customer_token=' . $this->session->data['customer_token']),
+				$this->url->link('account/order', 'language=' . $language . '&customer_token=' . $this->session->data['customer_token']),
+				$this->url->link('account/download', 'language=' . $language . '&customer_token=' . $this->session->data['customer_token']),
+				$this->url->link('information/contact', 'language=' . $language)
+			);
 		} else {
-			$data['text_message'] = sprintf($this->language->get('text_guest'), $this->url->link('information/contact', 'language=' . $this->config->get('config_language')));
+			$data['text_message'] = sprintf($this->language->get('text_guest'), $this->url->link('information/contact', 'language=' . $language));
 		}
 
-		$data['continue'] = $this->url->link('common/home', 'language=' . $this->config->get('config_language'));
+		$data['continue'] = $this->url->link('common/home', 'language=' . $language);
 
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['column_right'] = $this->load->controller('common/column_right');
