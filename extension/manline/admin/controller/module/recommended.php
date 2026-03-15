145151287a5c0d4a73e401fc470598c55bd4dccb
Valentyn Kotenko <kotenko@next-level.com>
2026-03-15 02:18:35 +0100

    Recommended module: load saved settings from oc_module.setting

--- extension/manline/admin/controller/module/recommended.php
+++ extension/manline/admin/controller/module/recommended.php
@@ -35,15 +35,23 @@ class Recommended extends \Opencart\System\Engine\Controller {
 		$data['user_token'] = $this->session->data['user_token'];
 
 		$module_info = [];
+		$module_setting = [];
 		if (isset($this->request->get['module_id'])) {
 			$this->load->model('setting/module');
 			$module_info = $this->model_setting_module->getModule((int)$this->request->get['module_id']);
+
+			if (!empty($module_info['setting'])) {
+				$decoded = json_decode($module_info['setting'], true);
+				if (is_array($decoded)) {
+					$module_setting = $decoded;
+				}
+			}
 		}
 
 		$data['module_id'] = isset($this->request->get['module_id']) ? (int)$this->request->get['module_id'] : 0;
 		$data['name'] = $module_info['name'] ?? $this->language->get('heading_title');
-		$data['status'] = $module_info['status'] ?? 0;
-		$data['blocks'] = $module_info['blocks'] ?? [];
+		$data['status'] = $module_setting['status'] ?? 0;
+		$data['blocks'] = $module_setting['blocks'] ?? [];
 
 		if (!is_array($data['blocks'])) {
 			$data['blocks'] = [];
